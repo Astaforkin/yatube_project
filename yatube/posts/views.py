e7751dc2191b8042ac6d13e3149bf027bda44f28
Nikita Astaforkin <astaforkin90@gmail.com>
2022-12-06 15:17:33 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 def index(request):    
     return HttpResponse('Главная страница')
 
+
 def group_posts(request, slug):
     return HttpResponse(f'Список posts {slug}')
